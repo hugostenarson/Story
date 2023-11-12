4abb79e19d07fe71e02ddd82890c0c0dfb3d68fa
hugostenarson <hugo.stenarson@elev.ga.ntig.se>
2023-11-12 21:49:35 +0100

    uppdaterade

--- Story/Program.cs
+++ Story/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine().ToLower();
+
+
+if (name == "Hugo" || name == "hugo")
+{
+    Console.WriteLine("Välkommen!");
+}
+else if (name == "Daniel" || name == "daniel")
+{
+    Console.WriteLine("Lite välkommen!");
+}
+else
+{
+    Console.WriteLine("Inte välkommen!");
+}
+
+// if (name != "Hugo")
+// {
+// Console.WriteLine("Fuck off!");
+// }
+
+Console.ReadLine();
